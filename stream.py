f779de22667592c87b701d5620385157216ad739
Nathanial George <ngeorge2@nd.edu>
2021-12-09 01:26:55 -0500

    Remove square white border on playpause button

--- stream.py
+++ stream.py
@@ -300,7 +300,7 @@ if __name__ == '__main__':
 
     # play button
     ppbutton1 = Button(frame1, image=playpause, bg="black",
-                       command=lambda: playorpause())
+                       command=lambda: playorpause(), bd = 0)
     ppbutton1.place(x=30, y=425)
 
     # subframe
@@ -342,7 +342,7 @@ if __name__ == '__main__':
 
     # play button
     ppbutton2 = Button(frame2, image=playpause, bg="black",
-                       command=lambda: playorpause())
+                       command=lambda: playorpause(), bd = 0)
     ppbutton2.place(x=30, y=425)
 
     # subframe
@@ -384,7 +384,7 @@ if __name__ == '__main__':
 
     # play button
     ppbutton3 = Button(frame3, image=playpause, bg="black",
-                       command=lambda: playorpause())
+                       command=lambda: playorpause(), bd = 0)
     ppbutton3.place(x=30, y=425)
 
     # subframe
@@ -426,7 +426,7 @@ if __name__ == '__main__':
 
     # play button
     ppbutton4 = Button(frame4, image=playpause, bg="black",
-                       command=lambda: playorpause())
+                       command=lambda: playorpause(), bd = 0)
     ppbutton4.place(x=30, y=425)
 
     # subframe
